fd97b91e52ef5cd515662985d09b6280e2922ad7
number9135 <navigate0425@gmail.com>
2023-10-26 20:31:13 +0900

    added StarRating Component

--- CheongJuApp/src/component/forms/PickerSelect.js
+++ CheongJuApp/src/component/forms/PickerSelect.js
@@ -1,78 +1,185 @@
-import { StyleSheet, Text, View, Modal, TouchableOpacity } from 'react-native'
-import React from 'react';
-import {widthPercentageToDP as wp, heightPercentageToDP as hp,} from "react-native-responsive-screen";
-import { Entypo } from '@expo/vector-icons';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Modal,
+  TouchableOpacity,
+  FlatList,
+  SafeAreaView,
+} from "react-native";
+import React, { useState } from "react";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { Entypo } from "@expo/vector-icons";
+import DataList from "../../../DataList.json";
+import { useNavigation } from "@react-navigation/core";
 
+const PickerSelect = ({ modalVisible, closeModal, onChangeMajorCategory, onChangeMediumCategory }) => {
 
-const PickerSelect = ({modalVisible, closeModal}) => {
+  const navigation = useNavigation();
 
-    
+  const [numColumns, setNumColumns] = useState(2);
+
+  const [isFocus, setIsFocus] = useState(null);
+
+  const handleButtonMajorPress = (button) => {
+    setIsFocus(button);
+    onChangeMajorCategory(button);
+  };
+
+  const handleButtonMediumPress = (button) => {
+    onChangeMediumCategory(button);
+    closeModal();
+  }
 
   return (
     <Modal visible={modalVisible} transparent={true}>
       <View style={styles.modalContainer}>
-        <View style={styles.bigCateory}>
-
+        <View style={styles.headerContainer}>
+          <Text style={styles.headerText}>카테고리를 분류하여 주세요</Text>
         </View>
-
-        <View style={styles.middleCategory}>
-
+        <View style={styles.cateContainer}>
+          <View style={styles.majorClassificationContainer}>
+            <View style={styles.detailCateContainer}>
+              <Text style={styles.detailCateText}>대분류</Text>
+            </View>
+            <TouchableOpacity
+              onPress={() => {
+                handleButtonMajorPress("음식")
+                }}
+              style={[
+                styles.majorCateButton,
+                isFocus === "음식" && styles.background,
+              ]}
+            >
+              <Text style={styles.majorCateText}>음식</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                handleButtonMajorPress("술집");
+              }}
+              style={[
+                styles.majorCateButton,
+                isFocus === "술집" && styles.background,
+              ]}
+            >
+              <Text style={styles.majorCateText}>술집</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                handleButtonMajorPress("까페&디저트");
+              }}
+              style={[
+                styles.majorCateButton,
+                isFocus === "까페&디저트" && styles.background,
+              ]}
+            >
+              <Text style={styles.majorCateText}>까페%디저트</Text>
+            </TouchableOpacity>
+          </View>
+          <SafeAreaView style={styles.mediumClassificationContainer}>
+            {isFocus === "음식" && (
+              <FlatList
+                data={DataList.FoodMediumClassification}
+                numColumns={numColumns}
+                showsVerticalScrollIndicator={false}
+                renderItem={({ item }) => (
+                  <TouchableOpacity onPress={()=>handleButtonMediumPress(`${item.title}`)}
+                  style={styles.mediumClassificationButton}>
+                    <Text>{item.title}</Text>
+                  </TouchableOpacity>
+                )}
+                keyExtractor={(item) => item.id}
+              />
+            )}
+          </SafeAreaView>
+          <View></View>
         </View>
-
-        <View style={styles.smallCategory}>
-
-        </View>
-
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity onPress={closeModal}
-          style={styles.buttonStyle}>
-            <Entypo name="cross" size={wp('5%')} color="black" />
-            <Text stle={styles.buttonText}>닫 기</Text>
-          </TouchableOpacity>
-        </View>
+        <TouchableOpacity onPress={closeModal}>
+          <Text>나가기</Text>
+        </TouchableOpacity>
       </View>
     </Modal>
   );
-}
+};
 
-export default PickerSelect
+export default PickerSelect;
 
 const styles = StyleSheet.create({
-    modalContainer : {
-        borderWidth : 1,
-        height : hp('70%'),
-        width : wp('90%'),
-        alignSelf : 'center',
-        top : 80,
-        backgroundColor : 'white'
-    },
+  modalContainer: {
+    borderWidth: 1,
+    height: hp("70%"),
+    width: wp("90%"),
+    alignSelf: "center",
+    top: 80,
+    backgroundColor: "white",
+  },
 
-    bigCateory : {
-        borderBottomWidth : 1,
-        height : hp('15%')
-    },
+  headerContainer: {
+    borderBottomWidth: 1,
+    height: hp("5%"),
+    justifyContent: "center",
+    alignItems: "center",
+  },
 
-    middleCategory : {
-        height : hp('25%'),
-        borderBottomWidth : 1,
-    },
+  headerText: {
+    fontSize: wp("4%"),
+    fontWeight: "400",
+  },
 
-    smallCategory : {
-        borderBottomWidth :  1,
-        height : hp('25%')
-    },
+  cateContainer: {
+    borderBottomWidth: 1,
+    height: hp("60%"),
+    flexDirection: "row",
+  },
 
-    buttonContainer : {
-        justifyContent :'center',
-        alignItems : 'center',
-        height : hp('5%')
-    },
+  majorClassificationContainer: {
+    borderRightWidth: 1,
+    width: wp("25%"),
+  },
 
-    buttonStyle : {
-        flexDirection : 'row'
-    },
+  detailCateContainer: {
+    borderBottomWidth: 1,
+    height: hp("5%"),
+    justifyContent: "center",
+    alignItems: "center",
+  },
 
-    buttonText : {
-        fontSize : wp('5%')
-    }
-})
+  detailCateText: {
+    fontSize: wp("4%"),
+  },
+
+  majorCateButton: {
+    borderBottomWidth: 1,
+    height: hp("5%"),
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  majorCateText: {
+    fontSize: wp("3.5%"),
+  },
+
+  background: {
+    backgroundColor: "gray",
+  },
+
+  mediumClassificationButton: {
+    borderWidth: 1,
+    height: hp("5%"),
+    width: wp("30%"),
+    margin: 5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  mediumClassificationText: {},
+
+  mediumClassificationContainer: {
+    width: wp("65%"),
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- CheongJuApp/src/component/page/WritingScreen.js
+++ CheongJuApp/src/component/page/WritingScreen.js
@@ -1,95 +1,236 @@
-import { View, Text, StyleSheet, TextInput } from 'react-native';
-import React, { useState } from 'react';
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
-import { TouchableOpacity } from 'react-native';
-import PickerSelect from '../forms/PickerSelect';
-
-
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  Image,
+  ScrollView
+} from "react-native";
+import React, { useState } from "react";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { TouchableOpacity } from "react-native";
+import PickerSelect from "../forms/PickerSelect";
+import { Entypo } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
 
 export default function WritingScreen() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [isMajorCategory, setIsMajorCategory] = useState("");
+  const [isMediumCategory, setIsMediumCategory] = useState("");
+  const [images, setImages] = useState([]);
+  const [isCamera, setIsCamera] = useState(false);
 
-const [modalVisible, setModalVisible] = useState(false)
+  const getPermission = async () => {
+    if (Platform.OS !== "web") {
+      const { status } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== "granted") {
+        alert("사진을 업로드하려면 사진첩 권한이 필요합니다.");
+        return false;
+      }
+      return true;
+    }
+  };
 
+  const removeImage = (index) => {
+    const newImages = [...images];
+    newImages.splice(index, 1);
+    setImages(newImages);
+  };
 
-  const [isTitle, setIsTitle] = useState('')
+  const handlerPickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [3, 2],
+      allowsMultipleSelection: true,
+      quality: 1,
+    });
 
-  const openModal = () =>{
-    setModalVisible(true)
-  }
+    console.log(result);
+
+    if (!result.canceled) {
+      const newImages = result.assets.map((asset) => asset.uri);
+      if (images.length + newImages.length <= 5) {
+        setImages([...images, ...newImages]);
+      } else {
+        alert("최대 5장까지 업로드 가능합니다.");
+      }
+    }
+  };
+
+  const [isTitle, setIsTitle] = useState("");
+
+  const openModal = () => {
+    setModalVisible(true);
+  };
 
   const closeModal = () => {
-    setModalVisible(false)
-  }
+    setModalVisible(false);
+  };
 
+  const hnadleMajorCategoryButton = (category) => {
+    setIsMajorCategory(category);
+  };
+
+  const hadnleMediumCategotyButton = (category) => {
+    setIsMediumCategory(category);
+  };
 
   return (
-    <View style={styles.container}>
+    <ScrollView showsVerticalScrollIndicator={false} Style={{flex:1}}>
+      <View style={styles.container}>
       <Text style={styles.headerText}>게시물 작성</Text>
       <TextInput
         value={isTitle}
         style={styles.titleTextInput}
         onChangeText={setIsTitle}
-        placeholder='제목을 입력하세요.'
-        fontSize={wp('4%')} />
-      <View style={styles.cateContainer}>
-        <TouchableOpacity onPress={openModal}
-        style={styles.cateButton}>
-          <Text style={styles.buttonText}>대분류</Text>
-        </TouchableOpacity>
+        placeholder="제목을 입력하세요."
+        fontSize={wp("4%")}
+      />
+      <View style={styles.photoContainer}>
+        {images.length > 0 &&
+          images.map((uri, index) => (
+         
+            <TouchableOpacity key={index} onPress={() => removeImage(index)}>
+              <Image source={{ uri }} style={styles.imageStyle} />
+            </TouchableOpacity>
+          
+          ))}
+        {images < 1 && (
+          <View style={{width:wp('80%'), height:hp('15%'), justifyContent: 'center', alignItems:'center'}}>
+          <TouchableOpacity
+            onPress={handlerPickImage}
+            style={styles.cameraButton}
+          >
+            <Entypo name="camera" size={wp("10%")} color="black" />
+            <Text style={{ fontSize: wp("4%") }}>사진을 담아 보세요</Text>
+          </TouchableOpacity>
+          </View>
+        )}
       </View>
-        <PickerSelect 
-        modalVisible={modalVisible}
-        closeModal={closeModal}/>
-    </View>
-  )
-}
+      <View style={{justifyContent:'flex-start', width:wp('80%'), marginTop:5,}}>
+      <Text style={{fontSize:wp('3%')}}>* 사진은 최대 5장까지 업로그 가능합니다.</Text>
+      <Text style={{fontSize:wp('3%')}}>* 업로드된 사진을 클릭하면 해당 사진이 삭제됩니다.</Text>
+      </View>
+      <View style={styles.cateContainer}>
+        <TouchableOpacity onPress={openModal} style={styles.cateButton}>
+          <Text style={styles.buttonText}>분 류</Text>
+        </TouchableOpacity>
+        <Text style={styles.selectedCateText}>{isMajorCategory}</Text>
+        <Text style={styles.selectedCateText}>{isMediumCategory}</Text>
+      </View>
 
+      <View style={styles.starContainer}>
+
+      </View>
+
+      <View style={styles.starContainer}>
+
+      </View>
+
+      <PickerSelect
+        onChangeMajorCategory={hnadleMajorCategoryButton}
+        onChangeMediumCategory={hadnleMediumCategotyButton}
+        modalVisible={modalVisible}
+        closeModal={closeModal}
+      />
+      </View>
+    </ScrollView>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems : 'center'
   },
   headerText: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+    marginTop : 20,
   },
   titleTextInput: {
-    width: wp('80%'),
-    height: hp('5%'),
+    width: wp("80%"),
+    height: hp("5%"),
     borderWidth: 1,
-    borderColor: 'gray',
-    marginVertical: hp('2%'),
-    paddingHorizontal: wp('2%'),
-    fontSize: wp('4%'),
+    borderColor: "gray",
+    marginVertical: hp("2%"),
+    paddingHorizontal: wp("2%"),
+    fontSize: wp("4%"),
   },
   cateContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginVertical: hp('2%'),
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    borderWidth: 0,
+    height: hp("7%"),
+    width: wp("80%"),
+    flexDirection: "row",
   },
   cateButton: {
-    width: wp('35%'),
-    height: hp('5%'),
+    width: wp("25%"),
+    height: hp("4%"),
     borderWidth: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   buttonText: {
-    fontSize: wp('3%'),
+    fontSize: wp("3%"),
   },
 
-  cateSelector : {
-    borderWidth : 1, 
-    width : wp('100%'),
-    flexDirection : 'row',
-    justifyContent : 'space-around'
+  cateSelector: {
+    borderWidth: 1,
+    width: wp("100%"),
+    flexDirection: "row",
+    justifyContent: "space-around",
   },
 
-  pickerStyle : {
-    width : wp('20%'),
-    height : hp('10%')
+  pickerStyle: {
+    width: wp("20%"),
+    height: hp("10%"),
+  },
+
+  selectedCateText: {
+    marginHorizontal: 15,
+    fontSize: wp("3%"),
+    backgroundColor: "cornsilk",
+    width: wp("20%"),
+    height: hp("4%"),
+    elevation: 3,
+    textAlign: "center",
+    textAlignVertical: "center",
+    opacity: 0.9,
+  },
+
+  photoContainer: {
+    borderWidth: 1,
+    height: hp("15%"),
+    width: wp("80%"),
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+
+  cameraButton: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  imageStyle: {
+    height: wp("20%"),
+    width: wp("20%"),
+    marginHorizontal: 10,
+  },
+
+  starContainer : {
+    borderWidth : 1,
+    width : wp('80%'),
+    height : hp('25%')
   }
 });
